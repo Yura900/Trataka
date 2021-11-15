48dfbae071112206fe8bd33ebde4a5c4cebc4fe4
Yura900 <70135440+Yura900@users.noreply.github.com>
2021-11-15 18:09:13 +0900

    refactoring with lucy

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
+from bs4 import BeautifulSoup
 from urllib2 import urlopen
-html = urlopen("https://www.5gcamp.com/")
 
-print(html.read())
+
+url = "https://www.5gcamp.com/"
+
+response = urlopen(url)
+soup = BeautifulSoup(response, "html.parser")
+elements = soup.find_all("div", {"class": "cont"})
+
+for element in elements:
+    subject = element.find("p", {"class": "subject"})
+    if subject:
+        print(subject.text)
